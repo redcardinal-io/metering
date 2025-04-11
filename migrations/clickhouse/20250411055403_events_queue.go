216bcd61fe94252377835d5bd68947d5246931d2
elliot alderson <elliot14A@localhost.localdomain>
2025-04-11 11:38:28 +0530

    feat(cmd): migrate clickhouse migration files to go

---
+++ migrations/clickhouse/20250411055403_events_queue.go
@@ -0,0 +1,57 @@
+package clickhouse
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/pressly/goose/v3"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	goose.AddMigrationContext(upEventsQueue, downEventsQueue)
+}
+
+func upEventsQueue(ctx context.Context, tx *sql.Tx) error {
+	// load envs for kafka
+	brokerList := viper.GetString("RCMETERING_KAFKA_BROKER_LIST")
+	if brokerList == "" {
+		return fmt.Errorf("RCMETERING_KAFKA_BROKER_LIST is not set")
+	}
+	topicList := viper.GetString("RCMETERING_KAFKA_TOPIC_LIST")
+	if topicList == "" {
+		return fmt.Errorf("RCMETERING_KAFKA_TOPIC_LIST is not set")
+	}
+	groupName := viper.GetString("RCMETERING_KAFKA_GROUP_NAME")
+	if groupName == "" {
+		return fmt.Errorf("RCMETERING_KAFKA_GROUP_NAME is not set")
+	}
+
+	_, err := tx.ExecContext(ctx, fmt.Sprintf(`
+		create table if not exists rc_events_queue(
+    		id String not null,
+    		type String not null,
+    		source String not null,
+    		source_metadata Map(String, String) not null,
+    		organization String not null,
+    		user String not null,
+    		timestamp DateTime not null,
+    		properties Map(String, String) not null,
+    		ingested_at DateTime default now(),
+    		validation_errors Map(String, String)
+		)
+		engine = Kafka()
+		settings
+    		kafka_broker_list = '%s',
+    		kafka_topic_list = '%s',
+    		kafka_group_name = '%s',
+    		kafka_format = 'JSONEachRow';
+	`, brokerList, topicList, groupName))
+	return err
+}
+
+func downEventsQueue(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.ExecContext(ctx, `drop table if exists rc_events_queue;`)
+	return err
+}
